499999f549a8164e79a9dcbf21777ece15d8027c
sburton42 <scotthburton@gmail.com>
2026-07-07 16:11:41 -0600

    Added swagger docs

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import router from './src/router.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' with { type: 'json' };
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     return res.status(200).json({ message: 'Server is running' });
--- src/router.js
+++ src/router.js
@@ -3,7 +3,39 @@ import { getBooksHandler, getBookByIdHandler } from './controllers/books.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Retrieve a list of books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: A list of books.
+ */
 router.get('/books', getBooksHandler);
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   get:
+ *     summary: Retrieve a single book by ID
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The book ID.
+ *     responses:
+ *       200:
+ *         description: A single book.
+ *       404:
+ *         description: Book not found.
+ */
 router.get('/books/:id', getBookByIdHandler);
 
 export default router;
